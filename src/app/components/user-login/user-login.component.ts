7c7b7b0f503c477616b16484142d733f8dc64be3
Elias Dahi <elias_dahi@hotmail.com>
2021-02-18 07:53:12 +0200

    Some Code to try the Users Service Methods.

--- src/app/components/user-login/user-login.component.ts
+++ src/app/components/user-login/user-login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { UsersService } from 'src/app/services/users.service';
+import { Users } from 'src/app/shared/AllClasses';
 
 @Component({
   selector: 'app-user-login',
@@ -7,9 +9,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserLoginComponent implements OnInit {
 
-  constructor() { }
+  public myUser: Users = new Users()
+  public myUsers: Users[] = [];
+  public errorMsgUsers: string = "";
+  public errorMsgUser: string = "";
+
+  constructor(private usersService: UsersService) { }
 
   ngOnInit(): void {
+
+    // this.myUser.id = "4";
+    // this.myUser.UserName = "Mohammed Rateb";
+    // this.myUser.Email = "mohammed_rateb@hotmail.com";
+
+
+    this.usersService.getUsers().subscribe(
+      (data) => {this.myUsers = data;},
+      (error) => {this.errorMsgUsers = error;}
+    );
+
+    this.usersService.getUserByID(1).subscribe(
+      (data) => {this.myUser = data;},
+      (error) => {this.errorMsgUser = error;}
+    );
+
+    // this.usersService.saveUser(this.myUser).subscribe(
+    //   (data) => {console.log(data);},
+    //   (error) => {this.errorMsgUser = error;}
+    // );
   }
 
 }
