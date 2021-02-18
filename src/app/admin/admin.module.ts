807a18931f680fdfe886bb989e2a0a3ab05b7f50
Elias Dahi <elias_dahi@hotmail.com>
2021-02-18 07:51:24 +0200

    Import The Admin Routing Module File.

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -1,16 +1,26 @@
+// Modules Imports
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { AdminRoutingModule } from "./admin-routing.module";
+
+// Components Import
 import { AdminLoginComponent } from './components/admin-login/admin-login.component';
 import { AdminDashboardComponent } from './components/admin-dashboard/admin-dashboard.component';
 import { FoodManageComponent } from './components/food-manage/food-manage.component';
 import { PeopleManageComponent } from './components/people-manage/people-manage.component';
+
+// Services Imports
+
+
+// Angular Materials Imports
 
 
 
 @NgModule({
   declarations: [AdminLoginComponent, AdminDashboardComponent, FoodManageComponent, PeopleManageComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    AdminRoutingModule
   ]
 })
 export class AdminModule { }
