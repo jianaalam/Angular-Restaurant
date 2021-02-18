cf8c32ae769ff56860732be1dea973c804278615
Elias Dahi <elias_dahi@hotmail.com>
2021-02-18 07:50:49 +0200

    Import the HTTP Module and Services .

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,10 @@
+// Modules Imports Here :
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule } from "@angular/common/http";
+
+// Components Imports Here :
 import { AppComponent } from './app.component';
 import { UserLoginComponent } from './components/user-login/user-login.component';
 import { HeaderComponent } from './components/header/header.component';
@@ -12,6 +15,19 @@ import { DefaultComponent } from './components/default/default.component';
 import { AddInvitationComponent } from './components/add-invitation/add-invitation.component';
 import { InvitationDetailsComponent } from './components/invitation-details/invitation-details.component';
 import { MealRequestComponent } from './components/meal-request/meal-request.component';
+
+// Services Imports Here :
+import { UsersService } from './services/users.service';
+import { UserLoginService } from './services/user-login.service';
+import { SignupService } from './services/signup.service';
+import { RestaurantService } from './services/restaurant.service';
+import { PersonService } from './services/person.service';
+import { MealService } from './services/meal.service';
+import { MealRequestService } from './services/meal-request.service';
+import { InvitationService } from './services/invitation.service';
+import { InitiativeService } from './services/initiative.service';
+import { HeaderService } from './services/header.service';
+import { ProcessHttpErrorMsgService } from './services/process-http-error-msg.service';
 
 @NgModule({
   declarations: [
@@ -28,9 +44,20 @@ import { MealRequestComponent } from './components/meal-request/meal-request.com
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [UsersService,
+    UserLoginService,
+    SignupService,
+    RestaurantService,
+    PersonService,
+    MealService,
+    MealRequestService,
+    InvitationService,
+    InitiativeService,
+    HeaderService,
+    ProcessHttpErrorMsgService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
